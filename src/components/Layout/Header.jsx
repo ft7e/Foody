b52cb66b6541062a098d6d5b136eba76c31353ac
ft7e <ibraheemhamodeh@gmail.com>
2022-10-15 17:06:04 +0300

    done the structure and header

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+import classes from './Header.module.css';
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Foody</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img
+          src={
+            'https://github.com/academind/react-complete-guide-code/blob/11-practice-food-order-app/code/02-adding-a-header-cmp/src/assets/meals.jpg?raw=true'
+          }
+          alt='A table full of delicious food!'
+        />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
