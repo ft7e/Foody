83a5172a48efe685134ae7e34bddca6e051fa799
ft7e <ibraheemhamodeh@gmail.com>
2022-10-22 17:04:59 +0300

    fully functioing

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -40,7 +40,7 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>{ctx.totalAmount.toFixed(2)} $</span>
+        <span>{Math.abs(ctx.totalAmount.toFixed(2))} $</span>
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={props.onClose}>
--- src/components/context/CartProvider.js
+++ src/components/context/CartProvider.js
@@ -33,10 +33,32 @@ const cartReducer = (state, action) => {
       };
 
     case 'REMOVE':
-      state.items.filter((item) => {
-        return item.id !== action.payload.item.id;
-      });
-      break;
+      const newTotalAmount =
+        state.totalAmount -
+        action.payload.item.price * action.payload.item.amount;
+
+      const indexOfItem = state.items.findIndex(
+        (item) => item.id === action.payload.item.id
+      );
+
+      let newItems;
+      if (state.items[indexOfItem].amount - action.payload.item.amount < 1) {
+        newItems = state.items.filter(
+          (item) => item.id !== action.payload.item.id
+        );
+      } else {
+        const targetItem = state.items[indexOfItem];
+        const newItem = {
+          ...targetItem,
+          amount: targetItem.amount - action.payload.item.amount,
+        };
+        newItems = [...state.items];
+        newItems[indexOfItem] = newItem;
+      }
+      return {
+        totalAmount: newTotalAmount,
+        items: newItems,
+      };
     default:
       return { ...state };
   }
